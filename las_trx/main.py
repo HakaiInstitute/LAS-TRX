93bd779066356c83b7b3b2998315f5ec86172fb6
Taylor Denouden <taylordenouden@gmail.com>
2022-04-12 13:11:55 -0700

    Add multiprocessing support

--- las_trx/main.py
+++ las_trx/main.py
@@ -1,28 +1,21 @@
-import math
 import os.path
 import sys
 from datetime import date
 from pathlib import Path
-from typing import Optional
 
-import laspy
-import numpy as np
-from PySide2.QtCore import QSize, QThread, Signal
+from PySide2.QtCore import QSize
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QApplication, QErrorMessage, QFileDialog, QMainWindow, QMessageBox
-from csrspy import CSRSTransformer
 from csrspy.enums import CoordType, Reference, VerticalDatum
 
 from las_trx.config import TransformConfig
 from las_trx.ui_mainwindow import Ui_MainWindow
 from las_trx.utils import REFERENCE_LOOKUP, VD_LOOKUP, sync_missing_grid_files, \
     utm_zone_to_coord_type
-from las_trx.vlr import GeoAsciiParamsVlr, GeoKeyDirectoryVlr
-
-CHUNK_SIZE = 10_000
+from las_trx.worker import TransformWorker
 
 
-def resource_path(relative_path):
+def resource_path(relative_path: str):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
@@ -44,9 +37,9 @@ class MainWindow(QMainWindow):
         self.err_msg_box.setWindowTitle("Error")
         self.help_msg_box = QMessageBox(self)
         self.help_msg_box.setText("Use '*' in the input file to select multiple files.\n"
-                                  "e.g. `C:\\\\path\\to\\files\*.laz`\n\n"
+                                  "e.g. `C:\\\\path\\to\\files\\*.laz`\n\n"
                                   "Use '{}' in the output file to generate a name from the input name.\n"
-                                  "e.g. `C:\\\\path\\to\\files\{}_nad83.laz`")
+                                  "e.g. `C:\\\\path\\to\\files\\{}_nad83.laz`")
         self.help_msg_box.setWindowTitle("Help")
 
         self.ui.toolButton_input_file.clicked.connect(self.handle_select_input_file)
@@ -64,13 +57,7 @@ class MainWindow(QMainWindow):
 
         self.dialog_directory = os.path.expanduser("~")
 
-        self.thread = TransformWorker()
-        self.thread.progress.connect(self.ui.progressBar.setValue)
-        self.thread.success.connect(self.on_process_success)
-        self.thread.error.connect(self.on_process_error)
-        self.thread.started.connect(lambda: self.ui.pushButton_convert.setEnabled(False))
-        self.thread.finished.connect(lambda: self.ui.pushButton_convert.setEnabled(True))
-        self.thread.finished.connect(lambda: self.ui.progressBar.setValue(0))
+        self.thread = None
 
         sync_missing_grid_files()
 
@@ -216,128 +203,16 @@ class MainWindow(QMainWindow):
         self.err_msg_box.exec()
 
     def convert(self):
-        self.thread.setup(self.transform_config, self.input_files, self.output_files)
+        self.thread = TransformWorker(self.transform_config, self.input_files, self.output_files)
+
+        self.thread.progress.connect(self.ui.progressBar.setValue)
+        self.thread.success.connect(self.on_process_success)
+        self.thread.error.connect(self.on_process_error)
+        self.thread.started.connect(lambda: self.ui.pushButton_convert.setEnabled(False))
+        self.thread.finished.connect(lambda: self.ui.pushButton_convert.setEnabled(True))
+        self.thread.finished.connect(lambda: self.ui.progressBar.setValue(0))
+
         self.thread.start()
-
-
-class TransformWorker(QThread):
-    started = Signal()
-    finished = Signal()
-    progress = Signal(int)
-    success = Signal()
-    error = Signal(BaseException)
-
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
-        self.config = None
-        self.input_files = None
-        self.output_files = None
-
-    def setup(self, config: TransformConfig, input_files: list[str], output_files: list[str]):
-        self.config = config
-        self.input_files = input_files
-        self.output_files = output_files
-
-    @staticmethod
-    def laz_backend(output_file: str) -> Optional[laspy.LazBackend]:
-        if output_file[-4:] == ".laz":
-            return laspy.LazBackend.Laszip
-
-    def get_total_iters(self) -> int:
-        total_iters = 0
-        for input_file in self.input_files:
-            with laspy.open(input_file) as in_las:
-                total_iters += math.ceil(in_las.header.point_count / CHUNK_SIZE)
-
-        return total_iters
-
-    def check_file_names(self):
-        for in_file, out_file in zip(self.input_files, self.output_files):
-            if in_file == out_file:
-                raise AssertionError("In path has as identical name to the output path. " \
-                                     "Change it to prevent overwriting the file.")
-
-        for i, out_file in enumerate(self.output_files[:-1]):
-            if out_file in self.output_files[i + 1:]:
-                raise AssertionError("Duplicate output file name detected. "
-                                     "Use a format string for the output path to output a file based on the stem of the "
-                                     r"corresponding input file. e.g. 'C:\\some\path\{}_nad83csrs.laz'")
-
-    def _do_transform(self):
-        self.check_file_names()
-
-        transformer = CSRSTransformer(**self.config.dict(exclude_none=True))
-        total_iters = self.get_total_iters()
-        current_iter = 0
-
-        for input_file, output_file in zip(self.input_files, self.output_files):
-            with laspy.open(input_file) as in_las:
-                new_header = in_las.header
-
-                # Update GeoKeyDirectoryVLR
-                # check and remove any existing crs vlrs
-                for crs_vlr_name in (
-                        "WktCoordinateSystemVlr",
-                        "GeoKeyDirectoryVlr",
-                        "GeoAsciiParamsVlr",
-                        "GeoDoubleParamsVlr",
-                ):
-                    try:
-                        new_header.vlrs.extract(crs_vlr_name)
-                    except IndexError:
-                        pass
-
-                new_header.vlrs.append(GeoAsciiParamsVlr.from_crs(self.config.t_crs))
-                new_header.vlrs.append(GeoKeyDirectoryVlr.from_crs(self.config.t_crs))
-                new_header.scales = np.array([0.01, 0.01, 0.01])
-                new_header.offsets = self.estimate_offsets(input_file, transformer)
-
-                with laspy.open(output_file, mode='w', header=new_header,
-                                laz_backend=self.laz_backend(output_file)) as out_las:
-                    for points in in_las.chunk_iterator(CHUNK_SIZE):
-                        # Convert the coordinates
-                        data = self._stack_dims(points)
-                        data = np.array(list(transformer(data)))
-
-                        # Create new point records
-                        points.change_scaling(offsets=new_header.offsets, scales=new_header.scales)
-                        points.x = data[:, 0]
-                        points.y = data[:, 1]
-                        points.z = data[:, 2]
-                        out_las.write_points(points)
-
-                        self.progress.emit(int(100 * (current_iter + 1) / float(total_iters)))
-                        current_iter += 1
-
-    @staticmethod
-    def _stack_dims(points):
-        # Copy the points for performance. Makes the underlying arrays contiguous
-        x = points.x.scaled_array().copy()
-        y = points.y.scaled_array().copy()
-        z = points.z.scaled_array().copy()
-        return np.stack((x, y, z)).T
-
-    def estimate_offsets(self, input_file: str, transformer: CSRSTransformer):
-        with laspy.open(input_file) as in_las:
-            points = next(in_las.chunk_iterator(CHUNK_SIZE))
-            data = self._stack_dims(points)
-
-            # Convert the coordinates
-            data = np.array(list(transformer(data)))
-
-            # Return estimated header offsets as min x,y,z of first batch
-            return np.min(data, axis=0)
-
-    def run(self):
-        self.started.emit()
-
-        try:
-            self._do_transform()
-            self.success.emit()
-        except Exception as e:
-            self.error.emit(e)
-
-        self.finished.emit()
 
 
 if __name__ == "__main__":
---
+++ las_trx/worker.py
@@ -0,0 +1,164 @@
+import math
+import multiprocessing
+from concurrent import futures
+from time import sleep
+
+import laspy
+import numpy as np
+from PySide2.QtCore import QThread, Signal
+from csrspy import CSRSTransformer
+from laspy import LasHeader
+
+from las_trx.config import TransformConfig
+from las_trx.vlr import GeoAsciiParamsVlr, GeoKeyDirectoryVlr
+
+CHUNK_SIZE = 10_000
+
+
+class TransformWorker(QThread):
+    started = Signal()
+    finished = Signal()
+    progress = Signal(int)
+    success = Signal()
+    error = Signal(BaseException)
+
+    def __init__(self, config: TransformConfig, input_files: list[str], output_files: list[str]):
+        super().__init__(parent=None)
+        self.config = config
+        self.input_files = input_files
+        self.output_files = output_files
+
+        self.total_iters = 0
+        for input_file in self.input_files:
+            with laspy.open(input_file) as in_las:
+                self.total_iters += math.ceil(in_las.header.point_count / CHUNK_SIZE)
+
+        self.pool = futures.ProcessPoolExecutor()
+        self.manager = multiprocessing.Manager()
+        self.lock = self.manager.RLock()
+        self.current_iter = self.manager.Value('i', 0)
+
+    def check_file_names(self):
+        for in_file in self.input_files:
+            if in_file in self.output_files:
+                raise AssertionError("One of in files matches name of output files. "
+                                     "Aborting because this would overwrite that input file.")
+
+        if len(self.output_files) != len(list(set(self.output_files))):
+            raise AssertionError("Duplicate output file name detected. "
+                                 "Use a format string for the output path to output a file based on the stem of the "
+                                 r"corresponding input file. e.g. 'C:\\some\path\{}_nad83csrs.laz'")
+
+    def _do_transform(self):
+        self.check_file_names()
+
+        config = self.config.dict(exclude_none=True)
+        futs = []
+        for input_file, output_file in zip(self.input_files, self.output_files):
+            fut = self.pool.submit(transform, config, input_file, output_file, self.lock, self.current_iter)
+            fut.add_done_callback(self.on_process_complete)
+            futs.append(fut)
+
+        while any([f.running() for f in futs]):
+            self.progress.emit(self.progress_val)
+            sleep(0.1)
+        self.progress.emit(self.progress_val)
+
+    @staticmethod
+    def on_process_complete(fut: futures.Future):
+        err = fut.exception()
+        if err is not None:
+            print(err)
+            raise err
+
+    @property
+    def progress_val(self):
+        return int(100 * self.current_iter.value / float(self.total_iters))
+
+    def run(self):
+        self.started.emit()
+
+        try:
+            self._do_transform()
+            self.success.emit()
+        except Exception as e:
+            self.error.emit(e)
+
+        self.finished.emit()
+
+
+def transform(config: dict, input_file: str, output_file: str, lock: multiprocessing.RLock, cur: multiprocessing.Value):
+    transformer = CSRSTransformer(**config)
+    config = TransformConfig(**config)
+
+    with laspy.open(input_file) as in_las:
+        new_header = in_las.header
+        new_header = clear_header_geokeys(new_header)
+        new_header = write_header_geokeys_from_crs(new_header, config.t_crs)
+        new_header = write_header_scales(new_header)
+        new_header = write_header_offsets(new_header, input_file, transformer)
+
+        laz_backend = laspy.LazBackend.Laszip if output_file[-4:] == ".laz" else None
+        with laspy.open(output_file, mode='w', header=new_header, laz_backend=laz_backend) as out_las:
+            for points in in_las.chunk_iterator(CHUNK_SIZE):
+                # Convert the coordinates
+                data = stack_dims(points)
+                data = np.array(list(transformer(data)))
+
+                # Create new point records
+                points.change_scaling(offsets=new_header.offsets, scales=new_header.scales)
+                points.x = data[:, 0]
+                points.y = data[:, 1]
+                points.z = data[:, 2]
+                out_las.write_points(points)
+
+                with lock:
+                    cur.value += 1
+
+
+def write_header_offsets(header: 'LasHeader', input_file: str, transformer: 'CSRSTransformer') -> 'LasHeader':
+    with laspy.open(input_file) as in_las:
+        points = next(in_las.chunk_iterator(CHUNK_SIZE))
+        data = stack_dims(points)
+
+        # Convert the coordinates
+        data = np.array(list(transformer(data)))
+
+        # Return estimated header offsets as min x,y,z of first batch
+        header.offsets = np.min(data, axis=0)
+    return header
+
+
+def clear_header_geokeys(header: 'LasHeader') -> 'LasHeader':
+    # Update GeoKeyDirectoryVLR
+    # check and remove any existing crs vlrs
+    for crs_vlr_name in (
+            "WktCoordinateSystemVlr",
+            "GeoKeyDirectoryVlr",
+            "GeoAsciiParamsVlr",
+            "GeoDoubleParamsVlr",
+    ):
+        try:
+            header.vlrs.extract(crs_vlr_name)
+        except IndexError:
+            pass
+
+    return header
+
+
+def write_header_geokeys_from_crs(header: 'LasHeader', crs: 'CRS') -> 'LasHeader':
+    header.vlrs.append(GeoAsciiParamsVlr.from_crs(crs))
+    header.vlrs.append(GeoKeyDirectoryVlr.from_crs(crs))
+    return header
+
+
+def write_header_scales(header: 'LasHeader') -> 'LasHeader':
+    header.scales = np.array([0.01, 0.01, 0.01])
+    return header
+
+
+def stack_dims(points: 'laspy.ScaleAwarePointRecord') -> 'np.array':
+    x = points.x.scaled_array().copy()
+    y = points.y.scaled_array().copy()
+    z = points.z.scaled_array().copy()
+    return np.stack((x, y, z)).T
